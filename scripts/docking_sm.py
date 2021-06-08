43dab64b4a5b47c593857139c2a2bb4dd9c713c1
tbohne <tbohne@uni-osnabrueck.de>
2021-06-08 14:25:59 +0200

    implement container detection / robot alignment transition

--- scripts/docking_sm.py
+++ scripts/docking_sm.py
@@ -3,7 +3,10 @@
 import rospy
 import smach
 import smach_ros
+import actionlib
 
+from mbf_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseActionGoal
+from geometry_msgs.msg import PoseStamped
 from arox_docking.msg import DockAction
 
 
@@ -15,27 +18,63 @@ class ContainerProximity(smach.State):
     @staticmethod
     def execute(userdata):
         rospy.loginfo('executing state CONTAINER_PROXIMITY')
+        # TODO: add gps check - correct area?
         return 'succeeded'
 
 
 class DetectContainer(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['succeeded', 'aborted'])
+        smach.State.__init__(self, outcomes=['succeeded', 'aborted'],
+                             input_keys=['detect_container_input'],
+                             output_keys=['detect_container_output'])
 
-    @staticmethod
-    def execute(userdata):
+        # TODO: subscribed to the 'detect_container_entry' node - check whether this is the best way to do that
+        self.subscriber = rospy.Subscriber('/container_entry', PoseStamped, self.callback)
+        self.received_goal = None
+
+    def callback(self, data):
+        self.received_goal = data
+
+    def execute(self, userdata):
         rospy.loginfo('executing state DETECT_CONTAINER')
-        return 'succeeded'
+
+        if self.received_goal:
+            rospy.loginfo("detected entry: %s", self.received_goal)
+            userdata.detect_container_output = self.received_goal
+            return 'succeeded'
+        return 'aborted'
 
 
 class AlignRobotToRamp(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['succeeded', 'aborted'])
+        smach.State.__init__(self, outcomes=['succeeded', 'aborted'],
+                             input_keys=['align_robot_to_ramp_input'],
+                             output_keys=['align_robot_to_ramp_output'])
 
     @staticmethod
     def execute(userdata):
         rospy.loginfo('executing state ALIGN_ROBOT_TO_RAMP')
-        return 'succeeded'
+
+        if userdata.align_robot_to_ramp_input:
+            rospy.loginfo("got userdata: %s", userdata.align_robot_to_ramp_input)
+
+            pose_stamped = userdata.align_robot_to_ramp_input
+            move_base_client = actionlib.SimpleActionClient("move_base_flex/move_base", MoveBaseAction)
+            move_base_client.wait_for_server()
+
+            goal = MoveBaseGoal()
+            goal.target_pose.header.frame_id = "base_link"
+            goal.target_pose.header.stamp = rospy.Time.now()
+            goal.target_pose.pose.position = pose_stamped.pose.position
+            goal.target_pose.pose.orientation = pose_stamped.pose.orientation
+
+            move_base_client.send_goal(goal)
+            rospy.loginfo("now waiting...")
+            move_base_client.wait_for_result()
+            rospy.loginfo("after wait: %s", move_base_client.get_result())
+            return 'succeeded'
+
+        return 'aborted'
 
 
 class DriveIntoContainer(smach.State):
@@ -82,7 +121,7 @@ class DockingStateMachine(smach.StateMachine):
     def __init__(self):
         super(DockingStateMachine, self).__init__(
             outcomes=['failed', 'docked'],
-            input_keys=['custom_goal'],
+            input_keys=['sm_input'],
             output_keys=[]
         )
 
@@ -92,13 +131,15 @@ class DockingStateMachine(smach.StateMachine):
                 'succeeded': 'DETECT_CONTAINER',
                 'aborted': 'failed'})
 
-            self.add('DETECT_CONTAINER', DetectContainer(), transitions={
-                'succeeded': 'ALIGN_ROBOT_TO_RAMP',
-                'aborted': 'failed'})
+            self.add('DETECT_CONTAINER', DetectContainer(),
+                     transitions={'succeeded': 'ALIGN_ROBOT_TO_RAMP',
+                                  'aborted': 'DETECT_CONTAINER'},
+                     remapping={'detect_container_output': 'sm_input'})
 
-            self.add('ALIGN_ROBOT_TO_RAMP', AlignRobotToRamp(), transitions={
-                'succeeded': 'DRIVE_INTO_CONTAINER',
-                'aborted': 'failed'})
+            self.add('ALIGN_ROBOT_TO_RAMP', AlignRobotToRamp(),
+                     transitions={'succeeded': 'DRIVE_INTO_CONTAINER',
+                                  'aborted': 'failed'},
+                     remapping={'align_robot_to_ramp_input': 'sm_input'})
 
             self.add('DRIVE_INTO_CONTAINER', DriveIntoContainer(), transitions={
                 'succeeded': 'LOCALIZE_CHARGING_STATION',
@@ -128,7 +169,7 @@ def main():
         wrapped_container=sm,
         succeeded_outcomes=['docked'],
         aborted_outcomes=['aborted'],
-        goal_key='custom_goal',
+        goal_key='sm_input',
         # result_key='result'
     )
 
