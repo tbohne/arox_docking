626d4f7ab509bff47a3488150ccedee9255cd787
tbohne <tbohne@uni-osnabrueck.de>
2021-07-05 15:28:41 +0200

    implement basic result communication for smach execution

--- scripts/start_smach.py
+++ scripts/start_smach.py
@@ -7,7 +7,6 @@ from arox_docking.msg import DockAction
 
 
 def start_smach():
-
     rospy.init_node('start_smach')
 
     smach_client = actionlib.SimpleActionClient('dock_to_charging_station', DockAction)
@@ -16,8 +15,15 @@ def start_smach():
     smach_client.wait_for_server()
     smach_client.send_goal(goal_msg)
     rospy.loginfo("goal sent, wait for accomplishment")
+
+    # success just means that the smach execution has been successful, not the docking itself
     success = smach_client.wait_for_result()
-    rospy.loginfo("SMACH execution terminated successfully: %s", success)
+
+    if success:
+        rospy.loginfo("SMACH execution terminated successfully")
+        rospy.loginfo("result: %s", smach_client.get_result().result_state)
+    else:
+        rospy.loginfo("SMACH execution failed: %s", smach_client.get_goal_status_text())
 
     rospy.spin()
 
