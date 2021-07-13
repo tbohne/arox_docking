2d2ebff3425a5772a2da51f5bffaea3d74ed4d5a
tbohne <tbohne@uni-osnabrueck.de>
2021-07-13 11:06:26 +0200

    define demo setup (dock + wait + undock)

--- scripts/start_smach.py
+++ scripts/start_smach.py
@@ -9,21 +9,42 @@ from arox_docking.msg import DockAction
 def start_smach():
     rospy.init_node('start_smach')
 
-    smach_client = actionlib.SimpleActionClient('dock_to_charging_station', DockAction)
+    docking_client = actionlib.SimpleActionClient('dock_to_charging_station', DockAction)
     goal_msg = DockAction().action_goal
     goal_msg.goal = "custom_goal"
-    smach_client.wait_for_server()
-    smach_client.send_goal(goal_msg)
+    docking_client.wait_for_server()
+    rospy.loginfo("START DOCKING PROCEDURE..")
+    docking_client.send_goal(goal_msg)
     rospy.loginfo("goal sent, wait for accomplishment")
 
     # success just means that the smach execution has been successful, not the docking itself
-    success = smach_client.wait_for_result()
+    success = docking_client.wait_for_result()
 
     if success:
         rospy.loginfo("SMACH execution terminated successfully")
-        rospy.loginfo("result: %s", smach_client.get_result().result_state)
+        rospy.loginfo("DOCKING PROCEDURE FINISHED: %s", docking_client.get_result().result_state)
+
+        rospy.loginfo("sleeping a moment..")
+        rospy.sleep(10)
+
+        undocking_client = actionlib.SimpleActionClient('undock_from_charging_station', DockAction)
+        goal_msg = DockAction().action_goal
+        goal_msg.goal = "custom_goal"
+        undocking_client.wait_for_server()
+        rospy.loginfo("START UNDOCKING PROCEDURE..")
+        undocking_client.send_goal(goal_msg)
+        rospy.loginfo("goal sent, wait for accomplishment")
+
+        success = undocking_client.wait_for_result()
+
+        if success:
+            rospy.loginfo("SMACH execution terminated successfully")
+            rospy.loginfo("UNDOCKING PROCEDURE FINISHED: %s", docking_client.get_result().result_state)
+        else:
+            rospy.loginfo("SMACH execution failed: %s", docking_client.get_goal_status_text())
+
     else:
-        rospy.loginfo("SMACH execution failed: %s", smach_client.get_goal_status_text())
+        rospy.loginfo("SMACH execution failed: %s", docking_client.get_goal_status_text())
 
     rospy.spin()
 
